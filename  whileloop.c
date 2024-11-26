87c22440df6857d15d29ac6f15a2ac3fe2727e03
Prajwal2006-hub <prajwalprajwalmdg123@gmail.com>
2024-11-26 08:24:23 -0800

    Add files via upload

---
+++  whileloop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1; 
+    printf("Prajwal\n");
+    printf("The first 10 natural numbers are:\n");
+    while (i <= 10) { 
+        printf("%d\n", i);
+        i++;
+    }
+
+    return 0;
+}
