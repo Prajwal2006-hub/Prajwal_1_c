123010c937248b56f1f560a53a93df955916885e
Prajwal2006-hub <prajwalprajwalmdg123@gmail.com>
2024-11-26 23:13:48 -0800

    Add files via upload

---
+++ one_dim.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+  printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        printf("Element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    printf("\nThe elements in the array are:\n");
+    for (int i = 0; i < n; i++) {
+        printf("Element %d: %d\n", i + 1, arr[i]);
+    }
+
+    return 0;
+}
