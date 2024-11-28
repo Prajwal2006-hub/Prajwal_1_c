561d4841120e7d5c39faa26b7408188dceae2ae4
Prajwal2006-hub <prajwalprajwalmdg123@gmail.com>
2024-11-28 12:13:20 +0530

    functions_of_string

---
+++ length_of_string.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[100];
+    int length;
+
+    printf("Enter a string: ");
+    fgets(str, 100, stdin);
+
+    length = strlen(str) - 1; 
+
+    printf("Length of the string: %d\n", length);
+ return 0;
+}
